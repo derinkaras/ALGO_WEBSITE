11aaf30eec453bca1c7bf6b1de93d4e77c4c39a1
Derin <derinkaras@gmail.com>
2025-11-07 16:45:24 -0700

    Update mlb page

--- src/pages/Mlb.tsx
+++ src/pages/Mlb.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import ExecuteSimulation from "../services/ExecuteSimulation.ts"
 
 const Mlb = () => {
